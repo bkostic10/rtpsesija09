063f5f7eaf34add1ea5b87625f5ef0da7fc2c215
Baxter <Baxter@xpwindows7>
2010-05-01 13:07:58 +0200

    1 korak: kreiranje metoda koje se pozivaju u ToString metodi

--- Klase/XMLCvor.cs
+++ Klase/XMLCvor.cs
@@ -32,19 +32,47 @@ namespace Klase
 
         public override string ToString()
         {
-            string rezultat = new string(prazan);
-            rezultat += "<" + ImeTaga + ">";
+            StringBuilder rezultat = new StringBuilder();
+            kreirajOtvoreniTag(rezultat);
+            kreirajDecu(rezultat);
+            kreirajVrednost(rezultat);
+            kreirajZatvoreniTag(rezultat);
+            return rezultat.ToString();
+        }
 
+        public void kreirajOtvoreniTag(StringBuilder rez)
+        {
+            rez.Append("\n<");
+            rez.Append(ImeTaga);
+            rez.Append(">");
+        }
+        public void kreirajDecu(StringBuilder rez) 
+        {
             IEnumerator<XMLCvor> it = Deca.GetEnumerator();
             while (it.MoveNext())
             {
                 XMLCvor cvor = (XMLCvor)it.Current;
-                rezultat += "\n" + cvor.ToString();
+                //cvor.spajanjeMetoda(rez);
+                rez.Append(cvor.ToString());
             }
+        }
+        public void kreirajVrednost(StringBuilder rez)
+        {
             if (Vrednost != "")
-                rezultat += Vrednost;
-            rezultat += "</" + ImeTaga + ">";
-            return rezultat;
+                rez.Append(Vrednost);
+        }
+        public void kreirajZatvoreniTag(StringBuilder rez)
+        {
+            rez.Append("</");
+            rez.Append(ImeTaga);
+            rez.Append(">");
+        }
+        public void spajanjeMetoda(StringBuilder rezultat)
+        {
+            kreirajOtvoreniTag(rezultat);
+            kreirajDecu(rezultat);
+            kreirajVrednost(rezultat);
+            kreirajZatvoreniTag(rezultat);
         }
     }
 }
