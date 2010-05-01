96b64b08d0994f92fca2c804d97da2f52e6d7b1e
Baxter <Baxter@xpwindows7>
2010-05-01 12:47:28 +0200

    Problem

---
+++ Klase/XMLCvor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Klase
+{
+    public class XMLCvor
+    {
+        private string imeTaga;
+        private string vrednost;
+        private char[] prazan = new char[0];
+        private List<XMLCvor> deca = new List<XMLCvor>();
+
+        public List<XMLCvor> Deca
+        {
+            get { return deca; }
+            set { deca = value; }
+        }
+        
+        public string ImeTaga
+        {
+            get { return imeTaga; }
+            set { imeTaga = value; }
+        }
+
+        public string Vrednost
+        {
+            get { return vrednost; }
+            set { vrednost = value; }
+        }
+
+        public override string ToString()
+        {
+            string rezultat = new string(prazan);
+            rezultat += "<" + ImeTaga + ">";
+
+            IEnumerator<XMLCvor> it = Deca.GetEnumerator();
+            while (it.MoveNext())
+            {
+                XMLCvor cvor = (XMLCvor)it.Current;
+                rezultat += "\n" + cvor.ToString();
+            }
+            if (Vrednost != "")
+                rezultat += Vrednost;
+            rezultat += "</" + ImeTaga + ">";
+            return rezultat;
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Klase;
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLCvor cd = new XMLCvor();
+            cd.ImeTaga = "CD";
+            cd.Vrednost = "\n";
+            
+            XMLCvor naziv = new XMLCvor(); 
+            naziv.ImeTaga = "NAZIV";
+            naziv.Vrednost = "Californication";
+            
+            XMLCvor bend = new XMLCvor();
+            bend.ImeTaga = "BEND";
+            bend.Vrednost = "RHCP";
+            
+            XMLCvor zemlja = new XMLCvor();
+            zemlja.ImeTaga = "ZEMLJA";
+            zemlja.Vrednost = "USA";
+            
+            XMLCvor godina = new XMLCvor();
+            godina.ImeTaga = "GODINA";
+            godina.Vrednost = "1999";
+
+            cd.Deca.Add(naziv);
+            cd.Deca.Add(bend);
+            cd.Deca.Add(zemlja);
+            cd.Deca.Add(godina);
+
+            Console.WriteLine(cd.ToString());
+
+        }
+    }
+}
